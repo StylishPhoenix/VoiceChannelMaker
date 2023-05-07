ef54eaebc04798ba9e3df6ab1b699af8f23be41f
StylishPhoenix <an007567@gmail.com>
2023-05-06 19:06:12 -0500

    Update voiceChannel.js

--- voiceChannel.js
+++ voiceChannel.js
@@ -10,8 +10,8 @@ client.on('ready', async () => {
 });
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-    console.log('test');
   if (newState.channel && newState.channel.type === 'voice' && newState.channel.id === monitoredChannelId) {
+      console.log('test');
     newState.guild.channels.create('New Voice Channel', {
       type: 'voice',
       userLimit: 2,
