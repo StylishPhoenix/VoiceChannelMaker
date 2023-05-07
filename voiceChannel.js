335e01b97e85e3a69fd83c7d6346e7e03262cd9a
StylishPhoenix <an007567@gmail.com>
2023-05-07 09:41:47 -0500

    Update voiceChannel.js

--- voiceChannel.js
+++ voiceChannel.js
@@ -5,23 +5,27 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBit
 
 const monitoredChannelId = config.channelId;
 
-client.on('ready', async () => {
-    console.log(`Bot has connected to Discord!`);
-});
-
 client.on('voiceStateUpdate', async (oldState, newState) => {
   // Check if the user joined the monitored voice channel
-if (newState.channel && newState.channel.type === 2 && newState.channel.id === monitoredChannelId) {
-  newState.guild.channels.create({
-    name: 'Test Voice Channel',
-    type: 2,
-    parent: newState.channel.parent
-  }).then((channel) => {
-    newState.setChannel(channel);
-  });
-}
+  if (newState.channel && newState.channel.type === 2 && newState.channel.id === monitoredChannelId) {
+    const member = await newState.guild.members.fetch(newState.member.id);
+    let gameName = "New Voice Channel";
 
+    if (member.presence.activities.length > 0) {
+      const activity = member.presence.activities.find(act => act.type === 'PLAYING');
+      if (activity && activity.name) {
+        gameName = getValidChannelName(activity.name);
+      }
+    }
 
+    newState.guild.channels.create({
+      name: gameName,
+      type: 2,
+      parent: newState.channel.parent
+    }).then((channel) => {
+      newState.setChannel(channel);
+    });
+  }
 
   // Check if the user left any voice channel
   if (oldState.channel && oldState.channel.type === 'voice') {
@@ -32,5 +36,6 @@ if (newState.channel && newState.channel.type === 2 && newState.channel.id === m
     }
   }
 });
+
 
 client.login(config.token);
