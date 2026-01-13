780c38bab6de283ea963f7717f2167b9336b6a1a
StylishPhoenix <an007567@gmail.com>
2026-01-12 20:25:50 -0500

    Remove debug logs from voiceStateUpdate handler
    Removed debug console logs from voiceStateUpdate event.

--- voiceChannel.js
+++ voiceChannel.js
@@ -35,7 +35,6 @@ const createdChannels = new Set();
 client.on('voiceStateUpdate', async (oldState, newState) => {
   if (newState.channel && newState.channel.type === 2 && newState.channel.id === monitoredChannelId) {
     const member = newState.member;
-    console.log('Test');
     let gameName = getValidChannelName(); // Default
 
     // Use presence directly from the event
@@ -43,7 +42,6 @@ client.on('voiceStateUpdate', async (oldState, newState) => {
     if (presence && Array.isArray(presence.activities) && presence.activities.length > 0) {
       // Try to find a "Playing" activity
       const activity = presence.activities.find(act => act.type === 0 && act.name);
-        console.log('Test2');
       if (activity) {
         gameName = getValidChannelName(activity.name);
       } else {
